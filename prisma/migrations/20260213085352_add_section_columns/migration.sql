43398a1a101c6f8ed637b78875f8870f11423b3d
owf18916 <onne.wf@gmail.com>
2026-02-13 10:14:27 +0700

    fix(db): add missing owner and section columns to material_usages and production_outputs partitions
    - Add 'owner' (INTEGER NOT NULL) column to material_usages in partitioning setup
    - Add 'section' (VARCHAR(100)) column to material_usages in partitioning setup
    - Add 'owner' (INTEGER NOT NULL) column to production_outputs in partitioning setup
    - Add 'section' (VARCHAR(100)) column to production_outputs in partitioning setup
    
    Resolves: Missing columns not deployed to PostgreSQL from Prisma schema during db push
    - Columns were defined in schema.prisma but missing in SQL partitioning script
    - This caused the columns to not be created when running 'npm run deploy:sql'
    
    Related files: scripts/sql/01_setup_partitions.sql

--- prisma/migrations/20260213085352_add_section_columns/migration.sql
+++
@@ -1,7 +0,0 @@
--- Add section column to production_outputs table
-ALTER TABLE production_outputs
-ADD COLUMN section VARCHAR(100);
-
--- Add section column to material_usages table
-ALTER TABLE material_usages
-ADD COLUMN section VARCHAR(100);
